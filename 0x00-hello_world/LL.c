bf651b5d646e0ac9a9a4c2cb5bcfb77f7de5ceb6
root <root@DESKTOP-08B321K.localdomain>
2022-11-08 10:46:57 +0300

    more on C variable

---
+++ 0x00-hello_world/LL.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// demo while loop
+//
+int main(void)
+{
+int i = 0, j;
+while (i<6)
+{
+j = 0;
+while ( j < 3)
+{
+printf("2");
+j++;
+}
+printf("\n");
+i++;
+}
+return (0);
+}
